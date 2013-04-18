e95bfe8dfbfc95956bd5b11357f1fe3455f28034
Ben Golden <ben@sendgrid.com>
2013-04-18 12:09:22 -0500

    added git ignore

---
+++ asana/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asana::Application.config.secret_token = 'f9e241d5401d65f42b812e4c36574ba93fcc86111b0adf35d9540556d902cb33f410ff248402c70124d3574ef9cb33108e6bed73e565cf8896c7d627f446947d'
